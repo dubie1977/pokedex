8c13d0642983fc0a55cbc6ddd5e416e6fb4c27bc
Joseph DuBay <dubie1977@Josephs-iMac.local>
2016-08-06 20:00:26 -0700

    setup coco pods and alamofire

--- pokedex-by-dubay/Pokemon.swift
+++ pokedex-by-dubay/Pokemon.swift
@@ -12,6 +12,12 @@ import UIKit
 class Pokemon {
     private var _name: String!
     private var _pokedexId: Int!
+    private var _discription: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
     
     var name: String{
         return _name
@@ -23,6 +29,30 @@ class Pokemon {
     
     var image: UIImage{
         return UIImage(named: "\(_pokedexId)")!
+    }
+    
+    var discription: String{
+        return _discription
+    }
+    
+    var type: String{
+        return _type
+    }
+    
+    var defense: String{
+        return _defense
+    }
+    
+    var height: String{
+        return _height
+    }
+    
+    var weight: String{
+        return _weight
+    }
+    
+    var attack: String{
+        return _attack
     }
     
     init(name: String, pokedexId: Int){
